bdf684647c831b3b616dd7ff644f4c964b13af84
webnetkz <8gg001@gmail.com>
2020-06-25 16:26:50 +0600

    udpare

--- manualwork/templates/header.php
+++ manualwork/templates/header.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
 <header>
 	<div class="logo">
 		<a href="index.php">
@@ -6,10 +15,15 @@
     </div>
 	<div class="language">
 		<ul>
-			<li><a href="kz.php">Қаз</a>
+			<li>
+				<a href="kz.php">Қаз</a>
 			</li>
-			<li><a href="index.php">Рус</</li>
-			<li><a href="en.php">Eng</a></li>
+			<li>
+				<a href="index.php">Рус<a>
+			</li>
+			<li>
+				<a href="en.php">Eng</a>
+			</li>
 		</ul>
 	</div>
 	<div class="menu">
@@ -21,42 +35,10 @@
 				<li><a href="">Тарифный калькулятор</a></li>
 				<li><a href="">Служба контроля качества</a></li>
 				<li><a href="">Заключение договора</a></li>
-				<li><a href="">Сроки доставки</a></li>
-				<li><a href="">Инструкция iDocs</a></li>
 			</ul></div>
-			<div>
-				<h2>Тарифы и услуги</h2>
-				<ul>
-					<li><a href="">Экспресс доставка</a></li>
-					<li><a href="">Стандартная доставка</a></li>
-					<li><a href="">Внутригородская доставка</a></li>
-					<li><a href="">Международная доставка</a></li>
-					<li><a href="">Центральная Азия</a></li>
-					<li><a href="">Дополнительные услуги</a></li>
-					<li><a href="">Классификатор зон</a></li>
-					<li><a href="">Интернет-магазинам</a></li>
-				</ul>
-			</div>
-			<div>
-				<h2>Полезно знать</h2>
-				<ul>
-					<li><a href="">Руководство по услугам</a></li>
-					<li><a href="">Частые вопросы</a></li>
-					<li><a href="">Способы оплаты</a></li>
-					<li><a href="">Правила пересылки</a></li>
-					<li><a href="">Правила упаковки</a></li>
-					<li><a href="">Закон о Почте</a></li>
-					<li><a href="">Типовой договор</a></li>
-				</ul>
-			</div>
-			<div>
-				<h2>О компании</h2>
-				<ul>
-					<li><a href="">Все контакты</a></li>
-					<li><a href="">Наша деятельность</a></li>
-					<li><a href="">Галерея</a></li>
-				</ul>
-			</div>
+			<h2>Тарифы и услуги</h2>
+			<h2>Полезно знать</h2>
+			<h2>О компании</h2>
 		</nav>
 	</div>
 	<div class="contacts">
