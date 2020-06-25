7fd9e53fc04db10e375640c9c034c95b76981028
webnetkz <8gg001@gmail.com>
2020-06-25 16:31:49 +0600

    start new version 0.00000000001

--- manualwork_v1.1/templates/footer.php
+++ manualwork_v1.1/templates/footer.php
@@ -2,3 +2,6 @@
    <span>© 2003-2014 DWExpress</span>
    <p><a href="mailto:express@delivery.kz"></a></p>
 </footer>
+<script src="public/js/main.js"></script>
+</body>
+</html>
--- manualwork_v1.1/templates/header.php
+++ manualwork_v1.1/templates/header.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
 <header>
 	<div class="logo">
 		<a href="index.php">
@@ -6,10 +15,15 @@
     </div>
 	<div class="language">
 		<ul>
-			<li><a href="kz.php">Қаз</a>
+			<li>
+				<a href="kz.php">Қаз</a>
 			</li>
-			<li><a href="index.php">Рус</</li>
-			<li><a href="en.php">Eng</a></li>
+			<li>
+				<a href="index.php">Рус<a>
+			</li>
+			<li>
+				<a href="en.php">Eng</a>
+			</li>
 		</ul>
 	</div>
 	<div class="menu">
