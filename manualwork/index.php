cd62ce7709d31f736516c1e304e9d2ac389b3cf7
CaptainAdeg <66054408+CaptainAdeg@users.noreply.github.com>
2020-06-25 15:59:59 +0600

    first step

---
+++ manualwork/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nachalo</title>
+	</head>
+<body>
+<?php require_once "templates/header.php"; ?>
+	
+
+
+
+
+
+
+
+	
+<?php require_once "templates/footer.php"; ?>
+</body>
+</html>
---
+++ manualwork/templates/header.php
@@ -0,0 +1,34 @@
+<header>
+	<div class="logo">
+		<a href="index.php">
+			<img src="две.ai">
+		</a>
+    </div>
+	<div class="language">
+		<ul>
+			<li><a href="kz.php">Қаз</a>
+			</li>
+			<li><a href="index.php">Рус</</li>
+			<li><a href="en.php">Eng</a></li>
+		</ul>
+	</div>
+	<div class="menu">
+		<nav><div class="online_service"><h2>Онлайн сервисы</h2>
+			<ul>
+				<li><a href="">Личный кабинет</a></li>
+				<li><a href="">Вызов курьера</a></li>
+				<li><a href="">Заказ обратного звонка</a></li>
+				<li><a href="">Тарифный калькулятор</a></li>
+				<li><a href="">Служба контроля качества</a></li>
+				<li><a href="">Заключение договора</a></li>
+			</ul></div>
+			<h2>Тарифы и услуги</h2>
+			<h2>Полезно знать</h2>
+			<h2>О компании</h2>
+		</nav>
+	</div>
+	<div class="contacts">
+			<h4>87006033365</h4>
+			<h4>87006033365</h4>
+	</div>
+</header> 
