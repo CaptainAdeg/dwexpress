766a2dbd26abd92049b124e560a5c971ee9de374
CaptainAdeg <66054408+CaptainAdeg@users.noreply.github.com>
2020-06-29 17:16:30 +0600

    footer added

--- templates/header.php
+++ templates/header.php
@@ -4,9 +4,9 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<link rel="stylesheet" href="/public/css/style.css">
-	<link rel="stylesheet" href="/public/css/header.css">
-	<link rel="stylesheet" href="/public/css/footer.css">
+	<link rel="stylesheet" href="public/css/style.css">
+	<link rel="stylesheet" href="public/css/header.css">
+	<link rel="stylesheet" href="public/css/footer.css">
 </head>
 <body>
 <header>
@@ -28,9 +28,9 @@
 			</li>
 		</ul>
 	</div>
-	<div class="menu">
+	<!--<div class="menu">
 		<nav><div class="online_service"><h2>Онлайн сервисы</h2>
-			<ul>
+			<ul class="submenu">
 				<li><a href="">Личный кабинет</a></li>
 				<li><a href="">Вызов курьера</a></li>
 				<li><a href="">Заказ обратного звонка</a></li>
@@ -42,7 +42,7 @@
 			</ul></div>
 			<div>
 				<h2>Тарифы и услуги</h2>
-				<ul>
+				<ul class="submenu">
 					<li><a href="">Экспресс доставка</a></li>
 					<li><a href="">Стандартная доставка</a></li>
 					<li><a href="">Внутригородская доставка</a></li>
@@ -55,7 +55,7 @@
 			</div>
 			<div>
 				<h2>Полезно знать</h2>
-				<ul>
+				<ul class="submenu">
 					<li><a href="">Руководство по услугам</a></li>
 					<li><a href="">Частые вопросы</a></li>
 					<li><a href="">Способы оплаты</a></li>
@@ -67,16 +67,59 @@
 			</div>
 			<div>
 				<h2>О компании</h2>
-				<ul>
+				<ul class="submenu">
 					<li><a href="">Все контакты</a></li>
 					<li><a href="">Наша деятельность</a></li>
 					<li><a href="">Галерея</a></li>
 				</ul>
 			</div>
 		</nav>
-	</div>
+	</div>-->
+	<!--<nav>
+  <ul class="topmenu">
+    <li><a href="">Онлайн сервисы</a></li>
+    	<ul class="submenu">
+    		<li><a href="">Личный кабинет</a></li>
+    		<li><a href="">Вызов курьера</a></li>
+    		<li><a href="">Заказ обратного звонка</a></li>
+    		<li><a href="">Тарифный калькулятор</a></li>
+    		<li><a href="">Служба контроля качества</a></li>
+    		<li><a href="">Заключение договора</a></li>
+    		<li><a href="">Сроки доставки</a></li>
+    		<li><a href="">Инструкция iDocs</a></li>
+  		</ul>
+    <li><a href="">Тарифы и услуги</a></li>
+    	<ul class="submenu">
+    		<li><a href="">Экспресс доставка</a></li>
+     		<li><a href=""></a>Стандартная доставка</li>
+     		<li><a href="">Внутригородская доставка</a></li>
+     		<li><a href="">Международная доставка</a></li>
+     		<li><a href="">Центральная Азия</a></li>
+     		<li><a href="">Дополнительные услуги</a></li>
+     		<li><a href=""></a>Классификатор зон</li>
+     		<li><a href="">Интернет-магазинам</a></li>
+     	</ul>
+    <li><a href="">Полезно знать</a></li>
+    	<ul class="submenu">
+    		<li><a href="">Руководство по услугам</a></li>
+     		<li><a href="">Частые вопросы</a></li>
+     		<li><a href="">Способы оплаты</a></li>
+     		<li><a href="">Правила пересылки</a></li>
+     		<li><a href="">Правила упаковки</a></li>
+     		<li><a href="">Закон о Почте</a></li>
+     		<li><a href="">Типовой договор</a></li>
+
+     	</ul>
+    <li><a href="">О компании</a></li>
+    	<ul class="submenu">
+    		<li><a href="">Все контакты</a></li>
+     		<li><a href="">Наша деятельность</a></li>
+     		<li><a href="">Галерея</a></li>
+     	</ul>
+  </ul>
+</nav>
 	<div class="contacts">
 			<h4>87006033365</h4>
 			<h4>87006033365</h4>
-	</div>
+	</div>-->
 </header> 
