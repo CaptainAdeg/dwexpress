e4c1e8efc68fc9631d93bd9f15e2c5852996917c
CaptainAdeg <66054408+CaptainAdeg@users.noreply.github.com>
2020-07-09 20:14:56 +0600

    body menu updated

--- index.php
+++ index.php
@@ -4,83 +4,39 @@
 	<title>Nachalot</title>
 	</head>
 <body>
-<style type="text/css">
-		.wrapper {
-			display: grid;
-			grid-template-columns: repeat(4, 1fr);
-			grid-gap: 1em;
-			grid-auto-rows: minmax(200px, auto);
-			font-size: 25px;
-
-			
-		}
-		.wrapper > div {
-			border:5px solid #129BFF;
-			border-radius: 20px;
-			vertical-align: middle;
-			text-align: center;
-
-height		: auto;
-	
-	
-
-			
-		} 
-		.wrapper > div > a {
-			text-decoration: none;
-			color: black;
-
-
-		}
-
-		.info {
-			display: grid;
-			grid-template-columns: repeat(3, 1fr);
-			grid-gap: 2em;
-			grid-auto-rows: 300px;
-			font-size: 30px;
-		}
-		.info > div, h2 {
-			border: 5px solid #129BFF;
-			
-
-		}
-		.logos {
-			width: 100px;
-			align-items: center;
-
-		}
-		p {
-			text-align: center;		}
-		
-
-		
-	</style>
-	<div class="wrapper">
-		<div class="finding"><a href=""><img src="икона1-05.svg" class="logos"><p>Отслеживание<p></a></div>
-		<div class="calc"><a href="" ><img src="икона2-05.svg" class="logos"><p>Калькулятор</p></a></div>
-		<div class="call"><a href=""><img src="икона3-05.svg" class="logos"><p>Оставить звонок</p></a></div>
-		<div class="mycab"><a href=""><img src="икона4-05.svg" class="logos"><p>Личный кабинет</p></a></div>
-	</div>
-	<h2>Работая с нами вы получаете</h2>
-	<div class="info">
-		<div class="find">
-			
-			<p>Отслеживание</p></div>
-		<div class="box">
-			
-			<p>Надежность</p></div>
-		<div class="time">
-			
-			<p>Практичность</p></div>
-	</div>
 <?php require_once "templates/header.php"; ?>
 	
 
 
 
 <pre>
+<main>
+		<section>
 
+				<div class="function">
+				<div class="function-line">
+					<a href="#"><div class="square">
+						<span class="function-square"><img class="square-img" src="public/img/икона1-05.svg"></span>
+						<span class="square-text">Отслеживание</span>
+					</div>
+					<a href="#"><div class="square">
+						<span class="function-square"><img class="square-img" src="public/img/икона2-05.svg"></span>
+						<span class="square-text">Расчитать стоимость</span>
+					</div></a>
+					<a href="#"><div class="square">
+						<span class="function-square"><img class="square-img" src="public/img/икона3-05.svg"></span>
+						<span class="square-text">Обратный звонок</span>
+					</div></a>
+					<a href="#"><div class="square">
+						<span class="function-square"><img class="square-img" src="public/img/икона4-05.svg"></span>
+						<span class="square-text">Личный кабинет</span>
+					</div></a>
+			
+					
+			</div>
+
+		</section>
+	</main>
 
 
 
