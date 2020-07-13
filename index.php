a44a88cbad91058e83b00e2b132914150c6ab8ad
CaptainAdeg <66054408+CaptainAdeg@users.noreply.github.com>
2020-07-13 17:18:43 +0600

    slider added

--- index.php
+++ index.php
@@ -5,30 +5,47 @@
 	</head>
 <body>
 <?php require_once "templates/header.php"; ?>
-	
-
-
-
+	<link rel="stylesheet" href="public/js/main.js">
 <pre>
 <main>
+	<section>
+			<div class="slider">
+                <div id="img-container" class="images-container">
+                <img class="slider-item fade" src="public/img/slider.png" alt="">
+                <img class="slider-item fade" src="public/img/slider.png" alt="">
+                <img class="slider-item fade" src="public/img/slider.png" alt="">
+                <img class="slider-item fade" src="public/img/slider.png" alt="">
+            </div>
+            <div class="buttons-block">
+                <div id="prev-btn">&lt;</div>
+                <div id="next-btn">&gt;</div>
+            </div>
+            <div id="dots-block">
+                <button class="dot dot-active"></button>
+                <button class="dot"></button>
+                <button class="dot"></button>
+                <button class="dot"></button>
+            </div>
+        </div>
+</section>
 		<section>
 
 				<div class="function">
 				<div class="function-line">
 					<a href="#"><div class="square">
-						<span class="function-square"><img class="square-img" src="public/img/икона1-05.svg"></span>
+						<span class="function-square"><img class="square-img" src="public/img/icons/икона1-05.svg"></span>
 						<span class="square-text">Отслеживание</span>
 					</div>
 					<a href="#"><div class="square">
-						<span class="function-square"><img class="square-img" src="public/img/икона2-05.svg"></span>
+						<span class="function-square"><img class="square-img" src="public/img/icons/икона2-05.svg"></span>
 						<span class="square-text">Расчитать стоимость</span>
 					</div></a>
 					<a href="#"><div class="square">
-						<span class="function-square"><img class="square-img" src="public/img/икона3-05.svg"></span>
+						<span class="function-square"><img class="square-img" src="public/img/icons/икона3-05.svg"></span>
 						<span class="square-text">Обратный звонок</span>
 					</div></a>
 					<a href="#"><div class="square">
-						<span class="function-square"><img class="square-img" src="public/img/икона4-05.svg"></span>
+						<span class="function-square"><img class="square-img" src="public/img/icons/икона4-05.svg"></span>
 						<span class="square-text">Личный кабинет</span>
 					</div></a>
 			
@@ -36,6 +53,37 @@
 			</div>
 
 		</section>
+
+		<HR WIDTH="1030px" ALIGN="center" SIZE="5">
+
+		<h1 class="middle-text">Работая с нами вы получаете</h1>
+
+		<section>
+
+				<div class="info">
+				<div class="info-line">
+					<div class="monitor">
+						<span class="info-monitor"><img class="monitor-img" src="public/img/icons/Practice.svg" style="position: relative; left: 13px;"></span>
+						<span class="monitor-text">Практичность</span>
+						<p align="center">Для вашего удобства мы <br>принимаем заказы без <br> выходных</p>
+						
+					</div>
+					<div class="monitor">
+						<span class="info-monitor"><img class="monitor-img" src="public/img/icons/Trust.svg"></span>
+						<span class="monitor-text" style="position: relative; bottom: 40px;">Надежность</span>
+						<p align="center">Доставим посылки в <br> полной целостности и <br> сохранности </p> 
+						
+						
+					</div>
+					<div class="monitor">
+						<span class="info-monitor"><img class="monitor-img" src="public/img/icons/Find.svg"></span>
+						<span class="monitor-text" align="">Отслеживание</span>
+						 <p align="justify">Отслеживайте трекер вашей <br>посылки, будьте в курсе ее <br> передвижения</p>
+						
+					</div>	
+			</div>
+		</section>
+		
 	</main>
 
 
